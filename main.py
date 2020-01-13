d6bdb23ecc18d9ff39bf2da7c7f71b65b39a41fe
edevars <e.devars.dev@gmail.com>
2020-01-13 13:28:40 -0600

    :zap: Obtaining datasets

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from common import config
 import news_page_object as news
 import argparse
 import logging
 import re
+import datetime
+import csv
+
 from requests.exceptions import HTTPError
+from common import config
 from urllib3.exceptions import MaxRetryError
 
 logging.basicConfig(level=logging.INFO)
-
 
 is_well_formed_link = re.compile(r'^https?://.+/.+$')
 is_root_path = re.compile(r'^/.+$')
@@ -17,19 +19,33 @@ logger = logging.getLogger(__name__)
 def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
 
-    logging.info(f'\033[1;31;40m Beginning scraper for {host} \033[0;37;40m')
+    logging.info(f'\033[1;33;40m Beginning scraper for {host} \033[0;37;40m')
     homepage = news.HomePage(news_site_uid, host)
 
     articles = []
     for link in homepage.article_links:
         article = _fetch_article(news_site_uid, host, link)
         if article:
-            logger.info("Article fetched!!")
-            print(f"\033[1;32;40m {article.title} \033[0;37;40m")
+            logger.info(
+                f"\033[1;32;40m Article fetched!! {article.title} \033[0;37;40m")
             articles.append(article)
 
-    print(
-        f"\n \033[1;33;40mNUMERO DE ARTICULOS OBTENIDOS: {len(articles)} \033[0;37;40m")
+    _save_articles(news_site_uid, articles)
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = f"{news_site_uid}_{now}_articles.csv"
+
+    csv_headers = list(
+        filter(lambda property: not property.startswith('_'), dir(articles[0])))
+
+    with open(out_file_name, mode='w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
 
 
 def _fetch_article(news_site_uid, host, link):
@@ -41,10 +57,12 @@ def _fetch_article(news_site_uid, host, link):
     try:
         article = news.ArticlePage(news_site_uid, builded_link)
     except(HTTPError, MaxRetryError) as e:
-        logger.warning('Error Fetching the article', exc_info=False)
+        logger.warning(
+            '\033[1;31;40m Error Fetching the article \033[0;37;40m', exc_info=False)
 
     if article and not article.body:
-        logger.warning('Invalid article, There is no body')
+        logger.warning(
+            '\033[1;31;40m Invalid article, There is no body \033[0;37;40m')
         return None
 
     return article
