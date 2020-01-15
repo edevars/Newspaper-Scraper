2289a99ae37081881040e7fa0f2ee0dcc131f7d3
edevars <e.devars.dev@gmail.com>
2020-01-14 18:51:24 -0600

    :zap: Cleaning data in project

--- newspaper_recipe.py
+++ newspaper_recipe.py
@@ -2,6 +2,7 @@ import pandas as pd
 from urllib.parse import urlparse
 import argparse
 import logging
+import hashlib
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
@@ -15,6 +16,8 @@ def main(filename):
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
     df = _fill_missing_titles(df)
+    df = _generate_uid_for_rows(df)
+    df = _remove_new_lines_from_body(df)
     return df
 
 
@@ -56,6 +59,34 @@ def _fill_missing_titles(df):
                       )
 
     df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
+    return df
+
+
+def _generate_uid_for_rows(df):
+    logger.info('Genereating uids')
+    uids = (df
+            .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis=1)
+            .apply(lambda hash_object: hash_object.hexdigest())
+            )
+
+    df['uid'] = uids
+
+    df.set_index('uid', inplace=True)
+
+    return df
+
+
+def _remove_new_lines_from_body(df):
+    logger.info('Removing new lines from body')
+    stripped_body = (df
+                     .apply(lambda row: row['body'], axis=1)
+                     .apply(lambda body: list(body))
+                     .apply(lambda letters: list(map(lambda letter: letter.replace('\n', ' '), letters)))
+                     .apply(lambda letters: ''.join(letters))
+                     )
+
+    df['body'] = stripped_body
+
     return df
 
 
